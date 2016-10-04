fe9aebcadbd89c389328e052b2cad70f1bc6fe8b
Kendal Harland <kendaljharland@gmail.com>
2016-10-04 22:58:26 +0000

    Implement PUT method
    run dartfmt
    
    Allow PUT requests in test server
    
    Implement DELETE and PATCH. Allow both on test server

--- test/platform/common_utils.dart
+++ test/platform/common_utils.dart
@@ -7,6 +7,15 @@ const _echoUrl = 'http://localhost:9090';
 
 /// Runs a common test suite that assumes a pre-configured Seltzer.
 void runPlatformTests() {
+  test('should make a valid DELETE request', () async {
+    var response = await delete('$_echoUrl/das/fridge/lacroix').send().first;
+    expect(JSON.decode(response.payload), {
+      'method': 'DELETE',
+      'url': '/das/fridge/lacroix',
+      'data': '',
+    });
+  });
+
   test('should make a valid GET request', () async {
     var response = await get('$_echoUrl/flags.json').send().first;
     expect(JSON.decode(response.payload), {
@@ -16,11 +25,29 @@ void runPlatformTests() {
     });
   });
 
+  test('should make a valid PATCH request', () async {
+    var response = await patch('$_echoUrl/pants/up').send().first;
+    expect(JSON.decode(response.payload), {
+      'method': 'PATCH',
+      'url': '/pants/up',
+      'data': '',
+    });
+  });
+
   test('should make a valid POST request', () async {
     var response = await post('$_echoUrl/users/clear').send().first;
     expect(JSON.decode(response.payload), {
       'method': 'POST',
       'url': '/users/clear',
+      'data': '',
+    });
+  });
+
+  test('should make a valid PUT request', () async {
+    var response = await put('$_echoUrl/pants/on').send().first;
+    expect(JSON.decode(response.payload), {
+      'method': 'PUT',
+      'url': '/pants/on',
       'data': '',
     });
   });
